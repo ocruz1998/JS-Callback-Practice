d00c348913de35080f3e7b04766f9d26a1fce361
Osbaldo <ocruz1998.oc@gmail.com>
2024-03-20 15:24:03 -0700

    made character move

--- index.js
+++ index.js
@@ -1,27 +1,78 @@
-const inventory = newInventory()
+const inventory = newInventory();
 move(inventory).to(0, 0)
 
-const character = newImage('assets/green-character/static.gif')
+const character = newImage('assets/green-character/static.gif');
 let direction = null;
 let x = 100;
 let y = 250;
 
+
+function handleDirectionChange(direction) {
+if(direction === null){
+    character.src = 'assets/green-character/static.gif'
+}
+else if(direction === 'west'){
+    character.src = 'assets/green-character/west.gif'
+}
+else if(direction === 'north'){
+    character.src = 'assets/green-character/north.gif'
+}
+else if(direction === 'east'){
+    character.src = 'assets/green-character/east.gif'
+}
+else if(direction === 'south'){
+    character.src = 'assets/green-character/south.gif'
+}
+}
+
+function movewithArrowKeys(left, bottom, callback){
+    document.addEventListener('keydown', function(e){
+        if(e.repeat) return;
+    
+        if(e.key === 'ArrowLeft'){
+            direction = 'west'
+        }
+        if(e.key === 'ArrowUp'){
+            direction = 'north'
+        }
+        if(e.key === 'ArrowRight'){
+            direction = 'east'
+        }
+        if(e.key === 'ArrowDown'){
+            direction = 'south'
+        }
+        callback(direction);
+    });
+    
+    document.addEventListener('keyup', function(e){
+        direction = null
+        callback(direction);
+    });
+    
+}
+
+function moveCharacter() {
 if(direction === 'west'){
-    x = x - 1
+    x = x - 1;
 }
 if(direction === 'north'){
-    y = y + 1
+    y = y + 1;
 }
 if(direction === 'east'){
-    x = x + 1
+    x = x + 1;
 }
 if(direction === 'south'){
-    y = y - 1
+    y = y - 1;
 }
-character.style.left = x + 'px'
-character.style.bottom = y + 'px'
+character.style.left = x + 'px';
+character.style.bottom = y + 'px';
+}
 
-move(character).to(100, 250)
+movewithArrowKeys(100, 250, handleDirectionChange);
+setInterval(moveCharacter, 100);
+
+
+
 
 
 move(newImage('assets/tree.png')).to(200, 450)
